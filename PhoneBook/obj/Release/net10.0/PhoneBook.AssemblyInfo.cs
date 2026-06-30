9ca089d6fee277b6ffbd029d64015b6b5c24772b
hartunnoo <hartunnoo@gmail.com>
2026-06-30 04:04:34 +0000

    style: DIRECTORI HMO uppercase branding

--- PhoneBook/obj/Release/net10.0/PhoneBook.AssemblyInfo.cs
+++ PhoneBook/obj/Release/net10.0/PhoneBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhoneBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+975e9d454edca64db69d9094aa361c60b6128682")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+023dcee3ef1852f6352c425832cd864370311994")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhoneBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhoneBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
